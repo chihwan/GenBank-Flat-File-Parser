7137b5e1ab8d153a19516b738e2558297711cc3c
Jason Reilly <jdr0887@gmail.com>
2015-06-09 10:06:01 -0400

    initial commit

---
+++ src/main/java/org/renci/gbff/filter/SourceOrganismNameFilter.java
@@ -0,0 +1,25 @@
+package org.renci.gbff.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.renci.gbff.Filter;
+import org.renci.gbff.model.Sequence;
+
+public class SourceOrganismNameFilter implements Filter {
+
+    private String organism;
+
+    public SourceOrganismNameFilter(String organism) {
+        super();
+        this.organism = organism;
+    }
+
+    @Override
+    public boolean accept(Sequence sequence) {
+        if (sequence.getSource() != null && StringUtils.isNoneEmpty(sequence.getSource().getOrganism())
+                && sequence.getSource().getOrganism().contains(organism)) {
+            return true;
+        }
+        return false;
+    }
+
+}
